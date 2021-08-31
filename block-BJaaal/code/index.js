636d3647dd21a999ecd2a7c3d6435f8799689736
sumit chahar <chahar.sumit888@gmail.com>
2021-08-31 18:35:27 +0530

    Complete string template literal file

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -8,15 +8,25 @@
 let firstName = "Arya";
 let lastName = "Stark";
 
+const username = 'sumit';
+let bio = 'learning to code';
+
 /* Do the following using `+` operator
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
 
+alert('I am ' + username + ' and my bio is ' + bio);
+
+alert('I\'m ' + firstName + ' ' + lastName + ' of Winterfall');  
+
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+
+alert(`I am ${username} and my bio is ${bio}`);
+alert(`I\'m ${firstName} ${lastName} of Winterfall`);
 
 let amount = 200;
 const taxPercent = 10;
@@ -28,9 +38,13 @@ const taxPercent = 10;
   - Above message should be in two lines
 */
 
+alert(`the total amount is ${amount} \n the tax amount is ${(amount * taxPercent) / 100}`);
+
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
     the tax amount is [(amount * taxPercent) / 100]
   `
   - Above message should be in two lines
 */
+
+alert("The total amount is " + amount + "\n" + "the tax amount is " + ((amount * taxPercent) / 100));
